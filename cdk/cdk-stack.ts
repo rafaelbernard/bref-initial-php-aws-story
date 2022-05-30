ee6bfdd31f5c7f7751fc795dbfde6f77ba43c615
Rafael Bernard Araújo <rafael.araujo@initd.info>
2022-05-30 10:52:24 -0300

    Bump dependencies and manual PHP layer

--- cdk/cdk-stack.ts
+++ cdk/cdk-stack.ts
@@ -3,6 +3,8 @@ import { Construct } from 'constructs';
 import * as sqs from 'aws-cdk-lib/aws-sqs';
 import { ServerlessLaravel } from 'cdk-serverless-lamp';
 import * as path from 'path';
+import * as lambda from "aws-cdk-lib/aws-lambda";
+import { Code, LayerVersion, Runtime, Function as CdkFunction } from 'aws-cdk-lib/aws-lambda';
 
 export class CdkStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
@@ -17,7 +19,7 @@ export class CdkStack extends Stack {
 
     // // Get Bref layer ARN from https://runtimes.bref.sh/
     // // At this page, select correct Region and PHP version
-    // const phpRuntimeLayer = LayerVersion.fromLayerVersionArn(scope, 'php-81-fpm', 'arn:aws:lambda:us-east-1:209497400698:layer:php-81-fpm:19');
+    const phpRuntimeLayer = LayerVersion.fromLayerVersionArn(this, 'php-81-fpm', 'arn:aws:lambda:us-east-1:209497400698:layer:php-81-fpm:19');
     //
     // const myFunction = new Function(this, 'myfunction', {
     //   runtime: Runtime.PROVIDED, // for custom runtime
@@ -26,9 +28,18 @@ export class CdkStack extends Stack {
     //   layers: [phpRuntimeLayer],
     // });
 
+    const myFunction = new CdkFunction(this, 'php-lambda', {
+    //const myFunction = new lambda.Function(this, 'php-lambda', {
+        runtime: Runtime.PROVIDED, // for custom runtime
+        code: Code.fromAsset('../php'),
+        handler: 'serverless/index.php',
+        layers: [phpRuntimeLayer],
+    });
+
     const laravelBut = new ServerlessLaravel(this, 'serverless-laravel', {
       brefLayerVersion: 'arn:aws:lambda:us-east-1:209497400698:layer:php-81-fpm:19',
       laravelPath: path.join(__dirname, '../php'),
+      handler: myFunction
     })
 
     new CfnOutput(this, 'sqs', {
