13af49e4934831b546a4bf94cc7d5d05f4dc1de2
Rafael Bernard Araújo <rafael@bernard-araujo.com>
2023-02-20 13:05:44 -0300

    Part: 1 (#2)

---
+++ php/handlers/get.php
@@ -0,0 +1,33 @@
+<?php
+function fibonacci(int $n): float
+{
+    if ($n <= 1) {
+        return $n;
+    }
+
+    $n2 = 0;
+    $n1 = 1;
+
+    for ($i = 2; $i < $n; $i++) {
+        $n2_ = $n2;
+        $n2 = $n1;
+        $n1 = ($n1 + $n2_);
+    }
+
+    return $n2 + $n1;
+}
+
+return function ($request) {
+    $int = (int) ($request['queryStringParameters']['int'] ?? random_int(1, 300));
+
+    $responseBody = [
+        'response' => 'OK. Time: ' . time(),
+        'now' => date('Y-m-d H:i:s'),
+        'int' => $int,
+        'result' => fibonacci($int),
+    ];
+
+    $response = new \Symfony\Component\HttpFoundation\JsonResponse($responseBody);
+
+    return (new \Bref\Event\Http\HttpResponse($response->getContent(), $response->headers->all()))->toApiGatewayFormatV2();
+};
