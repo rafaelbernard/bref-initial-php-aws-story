24c07038e7d3c6bcefc9f0f9c179341b2f9e6713
Rafael Bernard Araújo <rafael.araujo@initd.info>
2023-03-16 16:24:23 -0300

    Add: Part 2

--- cdk/cdk-stack.ts
+++ cdk/cdk-stack.ts
@@ -1,31 +1,51 @@
-import { CfnOutput, Stack, StackProps } from 'aws-cdk-lib';
+import { CfnOutput, RemovalPolicy, Stack, StackProps } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 import { Code, Function as LambdaFunction, FunctionUrlAuthType, LayerVersion, Runtime } from 'aws-cdk-lib/aws-lambda';
 import { join } from 'path';
+import { Bucket } from 'aws-cdk-lib/aws-s3';
 
 export class CdkStack extends Stack {
 
   // Get Bref layer ARN from https://runtimes.bref.sh/
-  public static brefLayerFunctionArn = 'arn:aws:lambda:us-east-1:209497400698:layer:php-82:16';
+  public static brefLayerFunctionArn = 'arn:aws:lambda:us-east-1:209497400698:layer:php-82:19';
 
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
+    const stackPrefix = 'BrefStory';
+
     const layer = LayerVersion.fromLayerVersionArn(this, 'php-layer', CdkStack.brefLayerFunctionArn);
 
-    const getLambda = new LambdaFunction(this, 'get', {
+    const brefBucket = new Bucket(this, `${stackPrefix}Bucket`, {
+      autoDeleteObjects: true,
+      removalPolicy: RemovalPolicy.DESTROY,
+    });
+
+    const functionName = 'GetFibonacciImage';
+
+    const getLambda = new LambdaFunction(this, `${stackPrefix}${functionName}`, {
       layers: [layer],
       handler: 'get.php',
       runtime: Runtime.PROVIDED_AL2,
       code: Code.fromAsset(join(__dirname, `../assets/get`)),
-      functionName: 'part1-get',
+      functionName,
+      environment: {
+        BUCKET_NAME: brefBucket.bucketName,
+      },
     });
 
-    const fnUrl = getLambda.addFunctionUrl({authType: FunctionUrlAuthType.NONE});
+    brefBucket.grantReadWrite(getLambda);
+
+    const fnUrl = getLambda.addFunctionUrl({ authType: FunctionUrlAuthType.NONE });
 
     new CfnOutput(this, 'TheUrl', {
       // The .url attributes will return the unique Function URL
       value: fnUrl.url,
+    });
+
+    new CfnOutput(this, 'Bucket', {
+      // The .url attributes will return the unique Function URL
+      value: brefBucket.bucketName,
     });
   }
 }
---
+++ php/src/Application/PicsumPhotoService.php
@@ -0,0 +1,107 @@
+<?php
+
+namespace BrefStory\Application;
+
+use AsyncAws\S3\Exception\NoSuchKeyException;
+use AsyncAws\S3\S3Client;
+use Symfony\Contracts\HttpClient\Exception\ClientExceptionInterface;
+use Symfony\Contracts\HttpClient\Exception\RedirectionExceptionInterface;
+use Symfony\Contracts\HttpClient\Exception\ServerExceptionInterface;
+use Symfony\Contracts\HttpClient\Exception\TransportExceptionInterface;
+use Symfony\Contracts\HttpClient\HttpClientInterface;
+use Symfony\Contracts\HttpClient\ResponseInterface;
+
+class PicsumPhotoService
+{
+    public function __construct(
+        private readonly HttpClientInterface $httpClient,
+        private readonly S3Client $s3Client,
+        private readonly string $bucketName,
+    ) {
+    }
+
+    public function getJpegImageFor(int $imagePixels): array
+    {
+        try {
+            return $this->getImageFromBucket($imagePixels);
+        } catch (NoSuchKeyException) {
+            // do nothing
+        }
+
+        return $this->fetchAndSaveImageToBucket($imagePixels);
+    }
+
+    private function getImageFromBucket(int $imagePixels): ?array
+    {
+        $objectOutput = $this->s3Client->getObject([
+            'Bucket' => $this->bucketName,
+            'Key' => $this->metadataKeyFor($imagePixels),
+        ]);
+
+        return json_decode($objectOutput->getBody(), associative: true);
+    }
+
+    /**
+     * @param int $imagePixels
+     *
+     * @return array
+     * @throws ClientExceptionInterface
+     * @throws RedirectionExceptionInterface
+     * @throws ServerExceptionInterface
+     * @throws TransportExceptionInterface
+     */
+    private function fetchAndSaveImageToBucket(int $imagePixels): array
+    {
+        list($url, $response, $fetchedImage) = $this->fetchImage($imagePixels);
+
+        $this->saveImage($imagePixels, $fetchedImage);
+
+        return $this->createAndPutMetadata($url, $response, $imagePixels);
+    }
+
+    private function fetchImage(int $imagePixels): array
+    {
+        $response = $this->httpClient->request('GET', $url = "https://picsum.photos/{$imagePixels}");
+        $fetchedImage = $response->getContent();
+        return [$url, $response, $fetchedImage];
+    }
+
+    private function saveImage(int $imagePixels, mixed $fetchedImage): void
+    {
+        $this->s3Client->putObject([
+            'Bucket' => $this->bucketName,
+            'Key' => $this->imageKeyFor($imagePixels),
+            'Body' => $fetchedImage,
+        ]);
+    }
+
+    private function createAndPutMetadata(mixed $url, ResponseInterface $response, int $imagePixels): array
+    {
+        $metadata = [
+            'originalUrl' => $url,
+            'redirectedUrl' => $response->getInfo()['url'] ?? null,
+            'imageLocation' => $this->imageKeyFor($imagePixels),
+            'contentDisposition' => $response->getHeaders()['content-disposition'] ?? null,
+            'contentType' => $response->getHeaders()['content-type'] ?? null,
+            'created' => (new \DateTimeImmutable())->format('Y-m-d H:i:s'),
+        ];
+
+        $this->s3Client->putObject([
+            'Bucket' => $this->bucketName,
+            'Key' => $this->metadataKeyFor($imagePixels),
+            'Body' => json_encode($metadata),
+        ]);
+
+        return $metadata;
+    }
+
+    private function imageKeyFor(int $imagePixels): string
+    {
+        return "image/$imagePixels.jpg";
+    }
+
+    private function metadataKeyFor(int $imagePixels): string
+    {
+        return "metadata/$imagePixels.json";
+    }
+}
