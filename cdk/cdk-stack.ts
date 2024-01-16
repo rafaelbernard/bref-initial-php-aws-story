cc2ce13b22cf3da7f1571d4607cbc0daf6bc3ade
Rafael Bernard Araújo <rafael.araujo@initd.info>
2024-01-16 18:25:59 +1300

    Use bref constructs and PHP 8.3

--- cdk/cdk-stack.ts
+++ cdk/cdk-stack.ts
@@ -1,22 +1,16 @@
 import { CfnOutput, RemovalPolicy, Stack, StackProps } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-import { Code, Function as LambdaFunction, FunctionUrlAuthType, LayerVersion, Runtime } from 'aws-cdk-lib/aws-lambda';
+import { FunctionUrlAuthType, LayerVersion, Runtime } from 'aws-cdk-lib/aws-lambda';
 import { join } from 'path';
 import { Bucket } from 'aws-cdk-lib/aws-s3';
 import { packagePhpCode, PhpFunction } from "@bref.sh/constructs";
 
 export class CdkStack extends Stack {
 
-  // Get Bref layer ARN from https://runtimes.bref.sh/
-  // public static brefLayerFunctionArn = 'arn:aws:lambda:us-east-1:209497400698:layer:php-82:30';
-  public static brefLayerFunctionArn = 'arn:aws:lambda:us-east-1:534081306603:layer:php-82:57';
-
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
     const stackPrefix = 'BrefStory';
-
-    const layer = LayerVersion.fromLayerVersionArn(this, 'php-layer', CdkStack.brefLayerFunctionArn);
 
     const brefBucket = new Bucket(this, `${stackPrefix}Bucket`, {
       autoDeleteObjects: true,
@@ -25,34 +19,19 @@ export class CdkStack extends Stack {
 
     const functionName = 'GetFibonacciImage';
 
-    const getLambda = new LambdaFunction(this, `${stackPrefix}${functionName}`, {
-      layers: [layer],
+    const getLambda = new PhpFunction(this, `${stackPrefix}${functionName}`, {
       handler: 'get.php',
+      phpVersion: '8.3',
       runtime: Runtime.PROVIDED_AL2,
-      code: Code.fromAsset(join(__dirname, `../assets/get`)),
+      code: packagePhpCode(join(__dirname, `../assets/get`), {
+        exclude: ['test', 'tests'],
+      }),
       functionName,
       environment: {
         BUCKET_NAME: brefBucket.bucketName,
       },
       memorySize: 1024,
     });
-
-    // const getLambda = new PhpFunction(this, `${stackPrefix}${functionName}`, {
-    //   // layers: [layer],
-    //   handler: 'get.php',
-    //   runtime: Runtime.PROVIDED_AL2,
-    //   // code: Code.fromAsset(join(__dirname, `../assets/get`)),
-    //
-    //   code: packagePhpCode(join(__dirname, `../assets/get`), {
-    //     exclude: ['docs'],
-    //   }),
-    //
-    //   functionName,
-    //   environment: {
-    //     BUCKET_NAME: brefBucket.bucketName,
-    //   },
-    //   memorySize: 1024,
-    // });
 
     brefBucket.grantReadWrite(getLambda);
 
